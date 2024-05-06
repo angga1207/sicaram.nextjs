39dd385d02f94159b3028f392dc9116783d82769
angga1207 <63151469+angga1207@users.noreply.github.com>
2024-05-06 17:01:31 +0700

    Update Fix Dashboard

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -77,14 +77,22 @@ const Header = () => {
             if (!CurrentUser) {
                 serverCheck().then((res) => {
                     if (res.status == 'error') {
-                        showSweetAlert(
-                            'error',
-                            'Terjadi Kesalahan Server', 'Server tidak merespon! <br /> Silahkan untuk reload halaman?',
-                            'Reload',
-                            'Batal',
-                            () => {
-                                window.location.reload();
-                            });
+
+                        Swal.fire({
+                            title: 'Terjadi Kesalahan Server',
+                            text: 'Server tidak merespon! <br /> Silahkan untuk reload halaman?',
+                            icon: 'error',
+                            showCancelButton: true,
+                            confirmButtonText: 'Login Ulang',
+                            cancelButtonText: 'Reload',
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = '/login';
+                            }
+                            if (result.isDismissed) {
+                                // window.location.reload();
+                            }
+                        });
                     }
                 });
             }
