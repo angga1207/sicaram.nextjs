e9b1b4c74ede34ba69ce2a74e8ad613a20ae9b66
angga1207 <63151469+angga1207@users.noreply.github.com>
2025-01-08 15:58:47 +0700

    Update Sidebar.tsx

--- components/Layouts/Sidebar.tsx
+++ components/Layouts/Sidebar.tsx
@@ -18,7 +18,7 @@ import { faRust } from '@fortawesome/free-brands-svg-icons';
 import Tippy from '@tippyjs/react';
 
 const Sidebar = () => {
-    const APP_VERSION = "2.4c.1";
+    const APP_VERSION = "2.4c.5";
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
