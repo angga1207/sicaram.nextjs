ccc4dfcca2a7370fde161417282dd60f813e6ff3
angga1207 <63151469+angga1207@users.noreply.github.com>
2026-02-11 08:53:47 +0700

    2.7.2

--- components/Accountancy/Persediaan/BelanjaPersediaanUntukDijual.tsx
+++ components/Accountancy/Persediaan/BelanjaPersediaanUntukDijual.tsx
@@ -680,6 +680,7 @@ const BelanjaPersediaanUntukDijual = (data: any) => {
                             <DownloadButtons
                                 data={dataInput}
                                 endpoint='/accountancy/download/excel'
+                                uploadEndpoint={year != 2024 ? '/accountancy/upload/excel' : ''}
                                 params={{
                                     type: 'untuk_dijual',
                                     category: 'persediaan',
@@ -688,10 +689,10 @@ const BelanjaPersediaanUntukDijual = (data: any) => {
                                     year: year,
                                 }}
                                 afterClick={(e: any) => {
-                                    if (e === 'error') {
+                                    if (e[0] === 'error') {
                                         Swal.fire({
-                                            title: 'Download Gagal!',
-                                            text: 'Terjadi kesalahan saat mendownload file.',
+                                            title: 'Gagal!',
+                                            text: e[1] ? e[1] : 'Terjadi kesalahan saat proses berlangsung.',
                                             icon: 'error',
                                             showCancelButton: false,
                                             confirmButtonText: 'Tutup',
@@ -700,13 +701,16 @@ const BelanjaPersediaanUntukDijual = (data: any) => {
                                         return;
                                     } else {
                                         Swal.fire({
-                                            title: 'Download Berhasil!',
-                                            text: 'File telah berhasil didownload.',
+                                            title: e[1] === 'Downloaded' ? 'Download Berhasil!' : 'Upload Berhasil!',
+                                            text: e[1] === 'Downloaded' ? 'File berhasil diunduh.' : 'File berhasil diunggah.',
                                             icon: 'success',
                                             showCancelButton: false,
                                             confirmButtonText: 'Tutup',
                                             confirmButtonColor: '#00ab55',
                                         });
+                                        if (e[1] == 'Uploaded') {
+                                            _getDatas();
+                                        }
                                         return;
                                     }
                                 }}
